ef3bb33da89c4dc03dffbf01118bc8495ced8a26
Ravindra211192 <chandravindra21@yahoo.com>
2020-10-15 01:09:27 +0200

    Add support for the copy assignment operator
    CopyAssignmentOperato.cpp shows the implementation of copy assignment
    operator on objects.

---
+++ CopyAssignmentOperator.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+class Classgrades
+{
+    private:
+     string name;
+     int* Grades;
+
+    public:
+      Classgrades(string newName){
+          cout << "In Constructor" << endl;
+          name = newName;
+          Grades = new int[3];
+          for (int i = 0; i < 3 ; i++) { Grades[i] = 0; };
+      }
+      ~Classgrades(){
+          cout << "In Destructor" << endl;
+          delete [] Grades;
+      }
+
+      Classgrades& operator= (const Classgrades& newgrades){
+          cout << "In Copy assignment operator" << endl;
+          if (this != &newgrades){
+              //name = newgrades.name;
+              if(Grades != NULL) delete [] Grades;
+              Grades = new int[3];
+              for(int i = 0; i < 3; i++){
+                  Grades[i] = newgrades.Grades[i];
+              }
+          }
+      }
+
+      void SetGrade(int id, int grade){
+          Grades[id] = grade;
+      }
+
+      void PrintGrades(){
+          cout <<"Student " << name << " has grades: " << endl;
+          for (int i = 0; i < 3; i++){ cout << Grades[i] <<" " ; }
+      }
+};
+
+int main()
+{
+    Classgrades Student("Ravindra");
+    Student.SetGrade(0,98);
+    Student.PrintGrades();
+
+    Classgrades Student1("Dikshya");
+    Student1 = Student;
+    Student1.PrintGrades();
+
+}
