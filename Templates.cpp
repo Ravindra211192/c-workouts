8587f48332d83f8e9cb4ec452eafef82e170f6a8
Ravindra211192 <chandravindra21@yahoo.com>
2020-10-21 01:40:49 +0200

    Add .cpp file for Templates in C++
    Templates.cpp file contains the basic understanding of Templates in C++.
    Template provides a way to perform generic operations in a type-safe
    way. It provides a way to create generic classes that utilizes the
    different types/functions.

---
+++ Templates.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#define DEBUG
+#include <assert.h>
+#include <sstream>
+using namespace std;
+
+template <typename T>
+class MyClass
+{
+    private:
+     T Val;
+     
+    public:
+     void SetVal(const T& x) { Val = x;}
+     const T& GetVal() const { return(Val); }
+};
+
+template <typename T>
+T Square(const T& y)
+{
+    T ySquared = y * y;
+    return(ySquared);
+}
+
+int main()
+{
+#ifdef DEBUG
+   cout << "I'm doing debug test now!" << endl;
+#endif
+
+int x = 1;
+assert(x);
+
+int p =2 ;
+int psquared = Square(p);
+cout << p << " squared = " << psquared << endl;
+
+double q = 0.5;
+double qSquared = Square(q);
+cout << q << " squared =" << qSquared << endl;
+
+MyClass <int> I;
+I.SetVal(42);
+cout << " The set value is: " << I.GetVal() << endl;
+
+MyClass <double> Y;
+Y.SetVal(3.145);
+cout << " The Set value is: " << Y.GetVal() << endl;
+
+}
